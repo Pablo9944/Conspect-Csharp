9158834a7448b98090aa9efba964c12e983368e9
Павел Юров <Peyurov@gmail.com>
2022-04-01 16:44:33 +0300

    Пройдено

---
+++ 1.2 Преобразование типов данных/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2_Преобразование_типов_данных
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Не явное преобразование
+            
+            int a = 300;
+            double b = a;
+
+            //Явное преобразование
+            byte c = (byte)a; // ошибки не будет
+            //byte f = checked((byte)a); // checked - сделает проверку,если будет переполнение то будет ошибка.
+
+            double d = 1.2;
+            byte e = (byte)d;
+            int g = (byte)d;
+
+            //Parse - позволяет конвертировать строковые в числовые
+
+            string inputString = "2029";
+         // int intValue = inputString; // ошибка
+            int intValue = int.Parse(inputString); // 2029
+            
+
+            double doubleValue = double.Parse(inputString);
+            ulong ulongValue = ulong.Parse(inputString);
+            byte byteValue = byte.Parse(inputString); // ошибка
+            
+            //TryParse
+            
+            bool result = byte.TryParse(inputString, out byteValue);
+            Console.WriteLine(result);
+
+            // Convert - позволяет конвертировать данные любого типа в данные любого типа
+            long m = Convert.ToInt64(inputString);
+            Console.WriteLine(m);
+
+        }
+    }
+}
---
+++ 1.2 Преобразование типов данных/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("1.2 Преобразование типов данных")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1.2 Преобразование типов данных")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("be368a1e-9903-4093-afef-9d816686d952")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
