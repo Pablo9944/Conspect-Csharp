64aa8f91075757d7b75b376a945e82ec03a99145
Pavel Yurov <peyurov@gmail.com>
2022-04-05 17:19:20 +0300

    Добавил уроки

---
+++ 1.8  Оптимизация рутинных задач циклы/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._8__Оптимизация_рутинных_задач_циклы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.Write("Укажите сколько сгенерировать случайных чисел: ");
+            int count = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= count; i++)
+            {
+                Console.WriteLine("{0} - случйное число {1}",i,r.Next(-8,10));
+            }
+            //for(;;) выполняет бесконечное кол-во раз
+            for (; ; )
+            {
+                // чтобы выйти нужно break;
+            }
+            int a = 5, b = 7;
+            for (;a!=b;)
+            {
+
+            }
+           
+        }
+    }
+}
--- test/Program.cs
+++ test/Program.cs
@@ -12,8 +12,6 @@ namespace test
     {
         static void Main(string[] args)
         {
-        
-
         }
     }
 
