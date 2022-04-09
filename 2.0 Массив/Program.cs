c677567a2611c0393959f9e7931d637efaef11a2
Pavel Yurov <Peyurov@gmail.com>
2022-04-09 14:20:44 +0300

    Массивы

--- 2.0 Массив/Program.cs
+++ 2.0 Массив/Program.cs
@@ -13,8 +13,14 @@ namespace _2._0_Массив
 
             int[] myArray = { 1, 2, 4 };
             myArray.GetLength(0);
+            
+            int[] Arrays = { 1, 2, 4,4,5,6,7 };
 
-            int[][] array = new int[3][];
+            //int[][] array = new int[3][];
+
+            //Array.Clear(myArray, 3, 2); // удалит с 3 индекса 2 эелемента
+            Array.ConstrainedCopy(myArray, 2, Arrays, 2, 2); // копирует из одного массива в другой
+            Array.Sort(Arrays); // сортирует массим
 
         }
     }
