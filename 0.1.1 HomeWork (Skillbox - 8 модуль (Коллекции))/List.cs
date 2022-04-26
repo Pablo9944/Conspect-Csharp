8f00504d73778de4fd1be07e34bfe43e1ab70c05
Pavel Yurov <Peyurov@gmail.com>
2022-04-26 17:10:14 +0300

    UP

---
+++ 0.1.1 HomeWork (Skillbox - 8 модуль (Коллекции))/List.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._1._1_HomeWork__Skillbox___8_модуль__Коллекции__
+{
+    public class List
+    {
+        List<int> list = new List<int>();
+        Random r  = new Random();
+
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+
+        public List()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(r.Next(0, 100));
+            }
+
+        }
+   
+    
+        public void Remove()
+        {
+            List<int> listTemp = new List<int>();
+
+            
+            foreach (var e in list)
+            {
+                if (e<25 || e>50)
+                {
+                    listTemp.Add(e);
+                    
+                }
+       
+            }
+            list = listTemp;
+          
+
+
+        }
+    
+    
+        public void Print()
+        {
+            int count = 0;
+            foreach (var e in list)
+            {
+                Console.WriteLine(e);
+                count++;
+            }
+            this.count = count;
+        
+        }
+    
+    
+    }
+}
---
+++ 0.1.1 HomeWork (Skillbox - 8 модуль (Коллекции))/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._1._1_HomeWork__Skillbox___8_модуль__Коллекции__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Задание 1. Работа с листом 
+
+            /* Создайте лист целых чисел. 
+           Заполните лист 100 случайными числами в диапазоне от 0 до 100. 
+           Выведите коллекцию чисел на экран. 
+           Удалите из листа числа, которые больше 25, но меньше 50. 
+           Снова выведите числа на экран.*/
+
+
+            List l = new List();
+            l.Print();
+            Console.WriteLine($"Остаток = {l.Count}");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            Console.Clear();
+            l.Remove();
+            l.Print(); 
+            Console.WriteLine($"Остаток = {l.Count}");
+            Console.ReadKey();
+
+
+
+
+            #endregion
+        }
+    }
+}
